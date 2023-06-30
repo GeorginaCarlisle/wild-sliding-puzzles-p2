3d8ed728edf0892a4ca40f2cc4d49968a36daa31
Georgina Carlisle <georgina.carlisle@gmail.com>
2023-06-30 17:17:44 +0100

    Create function to scramble the game picture

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,10 +22,37 @@ function setGamePicture() {
   let numberOfTiles = tilePictures.length;
   // loop assigning each tile in turn an image, images applied in array order so that the correct image ends up in the correct tile
   for (let i = 0; i < numberOfTiles; i++) {
-    let tile = document.getElementById("tile-" + (i+1));
+    let tile = document.getElementById('tile-' + (i+1));
     tile.style.backgroundImage = tilePictures[i];
   }
   console.log("Game picture set");
+  scrambleGamePicture();
+}
+
+/**
+ * Called by setGamePicture
+ * Places each tile in a different grid position as per scrambleSetting
+ * ScrambleSetting has been tested to ensure the puzzle is solvable
+ */
+function scrambleGamePicture() {
+  // Array items specify a grid area (grid-row-start/grid-column-start/grid-row-end/column-row-end) 
+  let scrambleSetting = [
+    "2/1/3/2",
+    "3/2/4/3",
+    "1/2/2/3",
+    "3/3/4/4",
+    "2/2/3/3",
+    "1/3/2/4",
+    "3/1/4/2",
+    "1/1/2/2",
+    "2/3/3/4"
+  ]
+  // loop to give each tile in turn it's scrambled grid-area value
+  for (let i = 0; i < 9; i++) {
+    let tile = document.getElementById('tile-' + (i+1));
+    tile.style.gridArea = scrambleSetting[i];
+    console.log(tile);
+  }
 }
 
 
