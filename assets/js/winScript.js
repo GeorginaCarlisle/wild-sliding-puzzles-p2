90d25cbec6957e11984fb918397927045a48a3ee
Georgina Carlisle <georgina.carlisle@gmail.com>
2023-07-04 11:00:20 +0100

    Create function to load puzzle picture

--- assets/js/winScript.js
+++ assets/js/winScript.js
@@ -10,4 +10,23 @@ function locatePuzzleNumber() {
   let completedPuzzleNumbers = sessionStorage.getItem("completedPictures");
   let currentPuzzle = completedPuzzleNumbers.charAt(completedPuzzleNumbers.length -1);
   console.log("Puzzle completed: " + currentPuzzle); 
+  loadPuzzlePicture(currentPuzzle);
+}
+
+/**
+ * Called by locatePuzzleNumber
+ * currentPuzzle argument is used to select corrosponding puzzle picture
+ * Puzzle picture is then loaded to the DOM
+ */
+function loadPuzzlePicture(currentPuzzle) {
+  let puzzlePicture = ``;
+  switch (currentPuzzle) {
+    case "0":
+      puzzlePicture = `<img src="assets/images/Puffin/puffin-original.webp" alt="Image of a Puffin with a catch of fish in it's mouth. Image shows Puffin's head and brightly coloured orange beak." class="image">`;
+    break;
+    default: alert("puzzle completed not recoginsed, please contact developer");
+    break;
+  }
+  let pictureContainer = document.getElementById("original-picture");
+  pictureContainer.innerHTML = puzzlePicture;
 }
