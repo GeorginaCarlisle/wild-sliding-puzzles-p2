b5f4e27fda2d15891b880c9ada3418f9b31f6f09
Georgina Carlisle <georgina.carlisle@gmail.com>
2023-07-04 11:21:44 +0100

    Create function to load puzzle information

--- assets/js/winScript.js
+++ assets/js/winScript.js
@@ -12,6 +12,7 @@ function locatePuzzleNumber() {
   console.log("Puzzle completed: " + currentPuzzle); 
   loadPuzzlePicture(currentPuzzle);
   loadPuzzleName(currentPuzzle);
+  loadPuzzleInformation(currentPuzzle);
 }
 
 /**
@@ -48,4 +49,37 @@ function loadPuzzleName(currentPuzzle) {
   }
   let nameSpan = document.getElementById("subject");
   nameSpan.innerHTML = puzzleName;
+}
+
+/**
+ * Called by locatePuzzleNumber
+ * currentPuzzle argument is used to select corrosponding puzzle information
+ * Puzzle information is then loaded to the DOM
+ */
+function loadPuzzleInformation(currentPuzzle) {
+  let puzzleInformation = ``;
+  switch (currentPuzzle) {
+    case "0":
+      puzzleInformation = `
+        <p>With their colourful bills and black and white plumage, puffins are unmistakable visitors to our shores. 
+        But their nesting sites and food supply are under threat, and their UK population could plummet by up to 
+        90% in the next 30 years.</p> 
+        <p>Atlantic puffins spend most of their lives out at sea, resting on the water and diving for sandeels. 
+        They don’t normally dive very deep but can go down to an incredible 50 metres or more – the height of the 
+        Leaning Tower of Pisa.</p>
+        <p>When it’s time to breed, they head for dry land, and every spring the UK welcomes roughly 10% of the 
+        world’s puffin population. In large colonies, individuals reunite with their life-long mate and lay one 
+        precious egg, usually in a burrow or a rocky crevice. For six weeks they work together to keep the egg warm 
+        until the chick hatches.</p>
+        <p>A puffin chick, or ‘puffling,’ weighs just 40g when it first emerges. Six weeks later, it weighs about 320g. 
+        It takes a lot of food to grow this quickly and pufflings need plenty of energy-rich, oily fish such as sprat 
+        and sandeels. But the climate crisis warming our oceans means these cold-water fish are on the decline, a 
+        problem that’s being made worse by sandeel fisheries.</P>
+        <p>`;
+    break;
+    default: alert("puzzle completed not recoginsed, please contact developer");
+    break;
+  }
+  let nameSpan = document.getElementById("subject");
+  nameSpan.innerHTML = puzzleInformation;
 }
