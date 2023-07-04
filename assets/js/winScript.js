0eb3c108ce6b4c6984d17b682ed69448df96b1a6
Georgina Carlisle <georgina.carlisle@gmail.com>
2023-07-04 10:42:11 +0100

    Add code comments to locatePuzzleNumber function

--- assets/js/winScript.js
+++ assets/js/winScript.js
@@ -1,6 +1,10 @@
 // Load page before populating with puzzle picture and associated information
 window.onload = locatePuzzleNumber();
 
+/**
+ * Called on load
+ * Uses session storage data to locate the puzzleNumber of the last puzzle completed
+ */
 function locatePuzzleNumber() {
   // locate puzzleNumber for puzzle just completed
   let completedPuzzleNumbers = sessionStorage.getItem("completedPictures");
