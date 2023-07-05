ebc92c9aa23b0b7bd955213edbea4beaa9defa96
Georgina Carlisle <georgina.carlisle@gmail.com>
2023-07-05 20:25:13 +0100

    Create a separate script file for index and link

---
+++ assets/js/indexScript.js
@@ -0,0 +1,29 @@
+// Load page before adding event listeners to modal button
+window.onload = function() {
+  // Add click event listener to the Instructions button
+  let btnInstructions = document.getElementById("button-instructions");
+  btnInstructions.addEventListener('click', instructions);
+}
+
+/**
+ * Called when the Instructions button is clicked. Click event Listener activated on load.
+ * Instructions modal displayed block and visible. 
+ * Modal created following the steps given in 'How To - CSS/JS Modal' by W3 Schools https://www.w3schools.com/howto/howto_css_modals.asp
+ * Click event listener added to close symbol with anonymous function to hide modal.
+ * Tile click event listeners prevented from activating further code while modal displayed.
+ */
+function instructions() {
+  // Display modal
+  let modal = document.getElementById("modal-instructions");
+  modal.style.display = "block";
+  // Prevent tile click event listeners for activating further code
+  preventClick = true;
+  // Add click event listener to close symbol that changes display back to none and re-hides modal
+  let closeCross = document.getElementsByClassName("close")[0];
+  closeCross.onclick = function() {
+    // Allow click event listeners to activate further code
+    preventClick = false;
+    // Hide modal 
+    modal.style.display = "none";
+  }
+}
